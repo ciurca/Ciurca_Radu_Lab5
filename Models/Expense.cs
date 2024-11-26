b3daaaf2ed08130c6c82da9b58c04eda8de3224c
Radu Ciurca <raduciurca@gmail.com>
2024-11-26 19:28:28 +0200

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ciurca_Radu_Lab5.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Secret { get; set; }
+    }
+}
---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ciurca_Radu_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
